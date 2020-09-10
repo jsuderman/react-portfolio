966e0b78aa4f97ce32043598241525fb30e2a7d3
Justin Suderman <jsuderman1@gmail.com>
2020-09-09 21:27:37 -0400

    organize portfoliowith app taking to each page of portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import Projects from "./components/pages/projects";
+import Contact from "./components/pages/contact";
+import Resume from "./components/pages/resume";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/"  component={Home} />
+        <Route exact path="/about"  component={About} />
+        <Route exact path="/projects"  component={Projects} />
+        <Route exact path="/contact"  component={Contact}/>
+        <Route exact path="/resume"  component={Resume}/>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
