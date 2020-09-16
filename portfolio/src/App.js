a88d394808b27012ab97869ddf011ea431501caf
Justin Suderman <jsuderman1@gmail.com>
2020-09-16 00:07:11 -0400

    final

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./components/pages/About/about";
 import Projects from "./components/pages/Projects/projects";
 import Contact from "./components/pages/Contact/contact";
