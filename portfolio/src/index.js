48b6864430c782bdf0712c90eb314d8596c17860
Justin Suderman <jsuderman1@gmail.com>
2020-09-18 13:58:19 -0400

    updated on app

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,8 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router} from "react-router-dom";
 
 
-ReactDOM.render(
-    <Router>
+ReactDOM.render(<Router>
         <App />
     </Router>
     , document.getElementById('root'));
