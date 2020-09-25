f8d4797bf595ae3fd67e398a97805402ecdf995d
Justin Suderman <jsuderman1@gmail.com>
2020-09-25 14:28:06 -0400

    added

---
+++ build/precache-manifest.54cadcf227fe7c36587b7419fbd34cd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf57fe57ce92d0713d1178bab676d45",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "327774835a7aca74d086",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5178a74f7845c93ad3ba",
+    "url": "/react-portfolio/static/css/main.31c8da7f.chunk.css"
+  },
+  {
+    "revision": "327774835a7aca74d086",
+    "url": "/react-portfolio/static/js/2.c012eeac.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/react-portfolio/static/js/2.c012eeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5178a74f7845c93ad3ba",
+    "url": "/react-portfolio/static/js/main.d2ac0579.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "5417723ea765c5406c83e088a4fdc36f",
+    "url": "/react-portfolio/static/media/IMG_9628.5417723e.png"
+  },
+  {
+    "revision": "3ece4d7ca66108bd2e8bc6ec12757509",
+    "url": "/react-portfolio/static/media/Justin-Suderman.3ece4d7c.pdf"
+  }
+]);
