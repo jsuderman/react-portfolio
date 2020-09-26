5e4bea17e27cb96bc0ced666ae9a070716eee094
Justin Suderman <jsuderman1@gmail.com>
2020-09-25 20:24:42 -0400

    went back to router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/pages/About/about";
 import Projects from "./components/pages/Projects/projects";
 import Contact from "./components/pages/Contact/contact";
@@ -12,26 +12,28 @@ import "./App.css"
 
 
 
-class App extends Component {
-  render() {
+function App() {
 
-    return(
+  return (
+
+    <Router>
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+      </Switch>
+      <Foot />
+    </div>
+    </Router>
     
-      <div className="App">
-        <Navigation />
-        <Switch>
-          <Route path="/" component={About} />
-          <Route path="/about"  component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact"  component={Contact} />
-          <Route path="/resume"  component={Resume} />
-        </Switch>
-        <Foot />
-      </div>
-    );
+  );
 
-  };
-}
+};
+
 
 
 export default App;
